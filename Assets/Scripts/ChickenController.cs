2c6da0b427d1494d8154020ebb557f20cb0a7f1b
hatanakatakato <80793900+hatanakatakato@users.noreply.github.com>
2021-08-28 08:37:53 +0900

    ジャンプ実装
    ジャンプできるようにした

--- Assets/Scripts/ChickenController.cs
+++ Assets/Scripts/ChickenController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class ChickenController : MonoBehaviour
 {
+
+    private Rigidbody2D rb2D;
+    [SerializeField] float maxVelocity = 10f;
+
     void Start()
     {
-        
+        this.rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,16 +23,17 @@ public class ChickenController : MonoBehaviour
 
             if (touch.phase == TouchPhase.Began && touch.position.x > Screen.width / 2)
             {
-                //画面左タッチ時
-                Debug.Log("画面左タッチ");
-                this.transform.Translate(1, 0, 0);
+                //画面右タッチ時
+                Debug.Log("右タッチされた");
+                this.rb2D.velocity = new Vector2(4f, 10f);
             }
             else if(touch.phase == TouchPhase.Began && touch.position.x <= Screen.width / 2)
             {
-                //画面右タッチ時
-                Debug.Log("画面右タッチ");
-                this.transform.Translate(-1, 0, 0);
+                //画面左タッチ時
+                Debug.Log("左タッチされた");
+                this.rb2D.velocity = new Vector2(-4f, 10f);
             }
+
         }
 
     }
