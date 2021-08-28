71e79389ca77c38b478b2d5e9f6d2d5c0f7d13c8
hatanakatakato <80793900+hatanakatakato@users.noreply.github.com>
2021-08-28 11:02:11 +0900

    ジャンプ音実装
    ジャンプ音実装した

--- Assets/Scripts/ChickenController.cs
+++ Assets/Scripts/ChickenController.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class ChickenController : MonoBehaviour
 {
-
+    //コンポーネント宣言
     private Rigidbody2D rb2D;
+    private AudioSource audioSource;
+
+    //変数宣言
     [SerializeField] float maxVelocity = 10f;
+    [SerializeField] float jumpVectorX = 4f;
+    [SerializeField] float jumpVectorY = 10f;
+    public AudioClip sound1;
+    
 
     void Start()
     {
+        //コンポーネント取得
         this.rb2D = GetComponent<Rigidbody2D>();
+        this.audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.touchCount > 0)
@@ -25,13 +33,16 @@ public class ChickenController : MonoBehaviour
             {
                 //画面右タッチ時
                 Debug.Log("右タッチされた");
-                this.rb2D.velocity = new Vector2(4f, 10f);
+                this.rb2D.velocity = new Vector2(this.jumpVectorX, this.jumpVectorY);
+                audioSource.PlayOneShot(sound1);
+
             }
             else if(touch.phase == TouchPhase.Began && touch.position.x <= Screen.width / 2)
             {
                 //画面左タッチ時
                 Debug.Log("左タッチされた");
-                this.rb2D.velocity = new Vector2(-4f, 10f);
+                this.rb2D.velocity = new Vector2(-this.jumpVectorX, this.jumpVectorY);
+                audioSource.PlayOneShot(sound1);
             }
 
         }
