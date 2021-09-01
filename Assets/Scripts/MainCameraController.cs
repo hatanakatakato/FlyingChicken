36bedf1737dc5f2cdb8f61d68124bb82874343e7
hatanakatakato <80793900+hatanakatakato@users.noreply.github.com>
2021-09-01 23:37:42 +0900

    ゲーム画面のマルチデバイス対応
    カメラにスクリプトを書いてマルチデバイス対応した

--- Assets/Scripts/MainCameraController.cs
+++ Assets/Scripts/MainCameraController.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class MainCameraController : MonoBehaviour
 {
-    public Camera camera;
+    //自身のコンポーネント
+    public Camera myCamera;
+    //外部のコンポーネント
+    public Transform playerTransform;
+
+    //ベースのアスペクト比を16:9にする
     public float baseWidth = 9.0f;
     public float baseHeight = 16.0f;
+    //Playerよりカメラを高い位置におく
+    public float distanceHeight = 4.2f;
 
     void Awake()
     {
-        Debug.Log($"ベース維持開始:camera.orthographic = {this.camera.orthographicSize}");
         // ベース維持
         var scaleWidth = (Screen.height / this.baseHeight) * (this.baseWidth / Screen.width);
         var scaleRatio = Mathf.Max(scaleWidth, 1.0f);
-        this.camera.orthographicSize *= scaleRatio;
-        Debug.Log($"ベース維持終了:camera.orthographic = {this.camera.orthographicSize}");
+        this.myCamera.orthographicSize *= scaleRatio;
     }
+
+    private void Update()
+    {
+        //カメラをplayerに合わせて動かす
+
+        this.transform.position = new Vector3(0f, playerTransform.position.y + distanceHeight, -10f);
+    }
+
 }
